fafa07ee5797b38cd4d671368522d025bbc6318a
darinthompson <darint07@gmail.com>
2020-03-28 00:01:02 -0600

    finished rough draft of program

--- src/com/skilldistillery/FoodTruckProject/FoodTruck.java
+++ src/com/skilldistillery/FoodTruckProject/FoodTruck.java
@@ -1,10 +1,54 @@
 package com.skilldistillery.FoodTruckProject;
 
 public class FoodTruck {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private static int ID = 1;
+	private int uniqueID;
+	private String name;
+	private String foodType;
+	private double rating;
+	
+	public FoodTruck(String _name, String _foodtype, double _rating) {
+		uniqueID = ID;
+		name = _name;
+		foodType = _foodtype;
+		rating = _rating;
+		ID++;
 	}
-
+	
+	public int getID() {
+		return uniqueID;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getFoodType() {
+		return foodType;
+	}
+	
+	public double getRating() {
+		return rating;
+	}
+	
+	public void setName(String _name) {
+		name = _name;
+	}
+	
+	public void setFoodType(String _foodtype) {
+		foodType = _foodtype;
+	}
+	
+	public void setRating(double _rating) {
+		rating = _rating;
+	}
+	
+	public String toString() {
+		return ("ID: " + uniqueID + "\n" +
+			"Name: " + name + "\n" +
+			"Food Type: " + foodType + "\n" +
+			"Rating: " + rating);
+	}
+	
+	
 }
---
+++ src/com/skilldistillery/FoodTruckProject/FoodTruckMain.java
@@ -0,0 +1,98 @@
+package com.skilldistillery.FoodTruckProject;
+
+import java.util.Scanner;
+
+public class FoodTruckMain {
+	private static int MAX_FOOD_TRUCKS = 5;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		FoodTruck[] foodCaravan = new FoodTruck[MAX_FOOD_TRUCKS];
+		String spacing = "\n**************************\n";
+		double rating;
+
+		for (int i = 0; i < MAX_FOOD_TRUCKS; i++) {
+			FoodTruck ft;
+			String truckName;
+			String foodType;
+			System.out.print("Name of Truck (type 'quit' to stop adding trucks): ");
+			truckName = sc.nextLine();
+			if (!truckName.toLowerCase().equals("quit")) {
+				System.out.print("Food Style: ");
+				foodType = sc.nextLine();
+				System.out.print("Rating: ");
+				rating = sc.nextDouble();
+				ft = new FoodTruck(truckName, foodType, rating);
+				foodCaravan[i] = ft;
+				sc.nextLine();
+			} else {
+				break;
+			}
+		}
+		System.out.println(spacing);
+
+		while (true) {
+			System.out.println("1. Show all food trucks and data");
+			System.out.println("2. Show average rating of food trucks");
+			System.out.println("3. Show highest rated food truck");
+			System.out.println("4. quit");
+			System.out.print("Choice: ");
+			int userChoice = sc.nextInt();
+			System.out.println(spacing);
+			
+			switch(userChoice) {
+			case 1: 
+				System.out.println(spacing);
+				for (FoodTruck truck : foodCaravan) {
+					if (truck != null) {
+						System.out.println(truck.toString());
+						System.out.println(spacing);
+					} else {
+						break;
+					}
+				}
+				System.out.println(spacing);
+				break;
+			case 2: 
+				int sum = 0;
+				int numOfTrucks = 0;
+				System.out.println(spacing);
+				for (int i = 0; i < foodCaravan.length; i++) {
+					if(foodCaravan[i] != null) {
+						sum += foodCaravan[i].getRating();
+						numOfTrucks++;
+					} else {
+						break;
+					}
+				}				
+				System.out.println("Average rating: " + (sum/numOfTrucks) + " stars");
+				System.out.println(spacing);
+				break;
+			case 3: 
+				double maxRating = 0;
+				FoodTruck winningTruck = null;
+				for (int i = 0; i < foodCaravan.length; i++) {
+					if(foodCaravan[i] != null) {
+						if(foodCaravan[i].getRating() > maxRating) {
+							maxRating = foodCaravan[i].getRating();
+							winningTruck = foodCaravan[i];
+						}
+					} else {
+						break;
+					}
+				}
+				System.out.println(winningTruck.toString());
+				System.out.println(spacing);
+				break;
+			case 4:
+				System.out.println("Hope you had enough food truck food. Cheers!");
+				System.exit(0);
+			}
+		}
+
+	}
+}
+
+//			System.out.println(1);
+			
